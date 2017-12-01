1b8e6d6ddbe806c6c03a44b523ebb254cb9d788d
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-30 21:03:30 -0500

    enable password auth

--- vagrant/VagrantProvision/provision.sh
+++ vagrant/VagrantProvision/provision.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Ensure password authentication is turned on
+sudo sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+
 cat /etc/hosts
 sudo apt-get update
 sudo apt-get install -y net-tools wget ansible python
